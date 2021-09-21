880229745eb91d2546454b620bb5f7a55ae22e15
KenKen <kiennahe130771@fpt.edu.vn>
2021-09-21 17:32:05 +0700

    cm

--- controller/app.js
+++ controller/app.js
@@ -1,5 +1,41 @@
 import data from '../data/eventDAO.js';
 
-for(let item of data){
-    console.log(item);
+let $lstHeroTable = document.getElementsByTagName('tbody')[0];
+
+for (let item of data) {
+    let $id = document.createElement('td');
+    let $name = document.createElement('td');
+    let $strDate = document.createElement('td');
+    let $endDate = document.createElement('td');
+    let $place = document.createElement('td');
+    let $content = document.createElement('td');
+    let $detail = document.createElement('td');
+    let $del = document.createElement('td');
+
+    $id.innerHTML = item.id;
+    $name.innerHTML = item.Name;
+    $strDate.innerHTML = item.FisrtDay;
+    $endDate.innerHTML = item.LastDay;
+    $place.innerHTML = item.Location;
+    $content.innerHTML = item.Content;
+
+
+    let $btnDeltail = document.createElement('a');
+    $btnDeltail.innerHTML = 'Xem chi tiết';
+    $btnDeltail.href = "#";
+    let $btnDel = document.createElement('a');
+    $btnDel.href = "#";
+    $btnDel.innerHTML = 'Xóa';
+    $detail.append($btnDeltail);
+    $del.append($btnDel);
+
+
+    let $row = document.createElement('tr');
+    $row.append(
+        $id, $name, $strDate, $endDate, $place, $content, $detail, $del
+    );
+    console.dir($row);
+    $lstHeroTable.append($row)
+
+
 }
