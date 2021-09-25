260f5f41be183e995c079cad40dc196697b87f33
KenKen <kiennahe130771@fpt.edu.vn>
2021-09-25 21:52:53 +0700

    khum biết nói gì hết trơn -.-

--- controller/EventDetails_Timeline.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $().timelinr({
-        orientation: 	'vertical',
-        issuesSpeed: 	300,
-        datesSpeed: 	100,
-        arrowKeys: 		'true',
-        startAt:		3
-    })
-});
