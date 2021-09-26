0becce9855d470ca8c378e05aaffb07ff4d1a499
KenKen <kiennahe130771@fpt.edu.vn>
2021-09-26 20:40:42 +0700

    commit

--- controller/ListHistoryCharacter_admin.js
+++ controller/ListHistoryCharacter_admin.js
@@ -1,46 +1,62 @@
-import data from '../data/ListHero_data.js'
-
 let $listCharacter = document.getElementsByTagName('tbody')[0];
+async function getConversation() {
+    let response = await db.collection('characters').get();
 
-for (let item of data) {
+    for (let doc of response.docs) {
+        let id = doc.id; //get id
 
-    let $id = document.createElement('td')
-    $id.className = ''
-    let $name = document.createElement('td')
-    let $image = document.createElement('td')
-    let $title = document.createElement('td')
-    let $birthDay = document.createElement('td')
-    let $location = document.createElement('td')
-    let $content = document.createElement('td')
-    let $detail = document.createElement('td');
-    let $del = document.createElement('td');
+        let data = doc.data();
+        console.log(data.name);
 
-    $id.innerHTML = item.id;
-    $name.innerHTML = item.Name;
-    $image.innerHTML = item.image;
-    $title.innerHTML = item.title;
-    $birthDay.innerHTML = item.birthDay;
-    $location.innerHTML = item.location;
-    $content.innerHTML = item.content;
 
-    let $btnDeltail = document.createElement('a');
-    $btnDeltail.innerHTML = 'Xem chi tiết';
-    $btnDeltail.href = "#";
-    $btnDeltail.style.textDecoration = 'none';
-    $btnDeltail.className = 'text-warning'
-    let $btnDel = document.createElement('a');
-    $btnDel.href = "#";
-    $btnDel.innerHTML = 'Xóa';
-    $btnDel.style.textDecoration = 'none';
-    $btnDel.className = 'text-warning'
 
-    $detail.append($btnDeltail);
-    $del.append($btnDel);
+        let $name = document.createElement('td');
+        let $image = document.createElement('td');
+        let $title = document.createElement('td');
+        let $birthDay = document.createElement('td');
+        let $deathDay = document.createElement('td');
+        let $location = document.createElement('td');
+        let $content = document.createElement('td');
+        let $detail = document.createElement('td');
+        let $del = document.createElement('td');
 
-    let $row = document.createElement('tr');
-    $row.append(
-        $id, $name, $title, $birthDay, $location, $content, $detail, $del
-    );
-    console.dir($row);
-    $listCharacter.append($row)
+
+        $name.innerHTML = data.name;
+        $image.innerHTML = data.picture;
+        $title.innerHTML = data.title;
+        $birthDay.innerHTML = data.birth;
+        $deathDay.innerHTML = data.death;
+        $location.innerHTML = data.title;
+        $content.innerHTML = data.name;
+
+        let $btnDeltail = document.createElement('a');
+        $btnDeltail.innerHTML = 'Xem chi tiết';
+        $btnDeltail.href = "#";
+        $btnDeltail.style.textDecoration = 'none';
+        $btnDeltail.className = 'text-warning'
+        let $btnDel = document.createElement('a');
+        $btnDel.href = "#";
+        $btnDel.innerHTML = 'Xóa';
+        $btnDel.style.textDecoration = 'none';
+        $btnDel.className = 'text-warning'
+        $btnDel.onclick = () => {
+            //c1
+            doc.ref.delete();
+            //c2 *
+            // db.collection('...').doc(id).delete();
+            //get detail
+            // db.collection('...').doc(id).get();
+        }
+        $detail.append($btnDeltail);
+        $del.append($btnDel);
+
+        let $row = document.createElement('tr');
+        $row.append(
+            $name, $image, $title, $birthDay, $deathDay, $location, $detail, $del
+        );
+        console.dir($row);
+        $listCharacter.append($row)
+    }
 }
+
+getConversation();
--- controller/User_CharacterList.js
+++ controller/User_CharacterList.js
@@ -2,42 +2,51 @@ import data from '../data/ListHero_data.js';
 
 let $ul = document.getElementsByClassName('list')[0];
 
-for(let item of data) {
-    let $li = document.createElement('li');
-    $li.className = 'character mr-3 mb-3';
 
-    let $figure = document.createElement('figure');
-    $figure.className = 'snip1273';
 
-    let $img = document.createElement('img');
-    $img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Quang_Trung_statue_02.jpg/800px-Quang_Trung_statue_02.jpg';
-    $img.alt = 'sample72';
 
-    let $figcaption = document.createElement('figcaption');
-    
-    let $name = document.createElement('h4');
-    $name.innerHTML = 'Tên: '+ item.Name;
+async function getListCharacter() {
+    let response = await db.collection('characters').get();
+    for (let doc of response.docs) {
+        let data = doc.data();
 
-    let $job = document.createElement('p');
-    $job.innerHTML = 'Nghề nghiệp: ';
+        let $li = document.createElement('li');
+        $li.className = 'character mr-3 mb-3';
 
-    let $date = document.createElement('p');
-    $date.innerHTML = 'Ngày sinh - mất: ';
+        let $figure = document.createElement('figure');
+        $figure.className = 'snip1273';
 
-    let $countryside = document.createElement('p');
-    $countryside.innerHTML = 'Quê quán: ';
+        let $img = document.createElement('img');
+        $img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Quang_Trung_statue_02.jpg/800px-Quang_Trung_statue_02.jpg';
+        // $img.src = data.picture;
+        $img.alt = 'sample72';
 
-    let $general = document.createElement('p');
-    $general.innerHTML = 'Khái quát: ';
+        let $figcaption = document.createElement('figcaption');
 
-    $figcaption.append($name, $job, $date, $countryside, $general);
+        let $name = document.createElement('h4');
+        $name.innerHTML = 'Tên: ' + data.name;
 
-    let $link = document.createElement('a');
-    $link.href = '#link';
+        let $job = document.createElement('p');
+        $job.innerHTML = `Chức danh: ${data.title}`;
 
-    $figure.append($img, $figcaption, $link);
-    $li.append($figure);
-    $ul.append($li);
-    
+        let $date = document.createElement('p');
+        $date.innerHTML = `Ngày sinh - mất: ${data.birth}-${data.death}`;
+
+        let $countryside = document.createElement('p');
+        $countryside.innerHTML = `Quê quán: ${data.title}`;
+
+        let $general = document.createElement('p');
+        $general.innerHTML = `Khái quát: ${data.general}`;
+
+        $figcaption.append($name, $job, $date, $countryside, $general);
+
+        let $link = document.createElement('a');
+        $link.href = '#link';
+
+        $figure.append($img, $figcaption, $link);
+        $li.append($figure);
+        $ul.append($li);
+    }
 }
-console.log($ul);
+
+getListCharacter();
