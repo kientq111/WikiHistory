6d8b98f48d15f89b129ac1bcd422d0048941959c
KenKen <kiennahe130771@fpt.edu.vn>
2021-09-26 14:19:06 +0700

    Revert "cm controller"
    This reverts commit 3a7fbc3a84d31b7d60206a064df5b7033d9037ac.

--- controller/app.js
+++
@@ -1,9 +0,0 @@
-async function getConversation() {
-    let response = await db.collection('characters').get();
-    for (let doc of response.docs) {
-        let data = doc.data();
-        console.log(data);
-    }
-}
-
-getConversation();
