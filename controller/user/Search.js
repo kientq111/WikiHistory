de8ef61c0c83efa37fe25b4075b293c5c9691c97
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2021-10-01 06:58:39 +0700

    ha

---
+++ controller/user/Search.js
@@ -0,0 +1,15 @@
+//
+////
+let $btnSearch = document.getElementsByClassName('btn-search')[0];
+
+
+$btnSearch.onclick = async () => {
+    let $input = document.getElementsByClassName('search')[0].value;
+
+    let db_IdCharacter;
+
+
+
+    console.log($input);
+
+}
