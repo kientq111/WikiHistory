f8b5d0208d3dec032277ef98f2f20c9580d59a03
Alex <alexnguyen123229@gmail.com>
2021-09-30 20:51:18 +0700

    dat commit

---
+++ view/ValidateForm/Login-Register/firebase.js
@@ -0,0 +1,17 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBZHIGW83aKAXXI1nMlyIvWQH3Z8Qv0oKk",
+
+    authDomain: "signup-b3517.firebaseapp.com",
+
+    projectId: "signup-b3517",
+
+    storageBucket: "signup-b3517.appspot.com",
+
+    messagingSenderId: "68036495048",
+
+    appId: "1:68036495048:web:b2cd41f4eb05440e4c07fd"
+
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
