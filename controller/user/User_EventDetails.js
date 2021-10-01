fba4cb52448332100b7e2c96c331a5e1975a118e
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2021-10-01 08:54:06 +0700

    ha

--- controller/user/User_EventDetails.js
+++ controller/user/User_EventDetails.js
@@ -1,45 +1,88 @@
 import data from '../../data/eventDAO.js'
 
-//Timeline
-//dates
-let $ulDates = document.getElementsByClassName('dates')[0];
+// //Timeline
+// //dates
+// let $ulDates = document.getElementsByClassName('dates')[0];
 
-for (let item of data) {
-    let $liDates = document.createElement('li');
+// for (let item of data) {
+//     let $liDates = document.createElement('li');
 
-    let $aDates = document.createElement('a');
-    $aDates.href = '#' + '1900';
-    $aDates.innerHTML = '1900';
-    // $aDates.className = 'selected';
+//     let $aDates = document.createElement('a');
+//     $aDates.href = '#' + '1900';
+//     $aDates.innerHTML = '1900';
+//     // $aDates.className = 'selected';
 
-    $liDates.append($aDates);
-    $ulDates.append($liDates);
-}
+//     $liDates.append($aDates);
+//     $ulDates.append($liDates);
+// }
 
 
 
-//Issues
-let $ulIssues = document.getElementsByClassName('issues')[0];
+// //Issues
+// let $ulIssues = document.getElementsByClassName('issues')[0];
 
-for (let item of data) {
-    let $liIssues = document.createElement('li');
-    $liIssues.id = '1990';
+// for (let item of data) {
+//     let $liIssues = document.createElement('li');
+//     $liIssues.id = '1990';
 
-    let $imgIssues = document.createElement('img');
-    $imgIssues.src = item.Image;
+//     let $imgIssues = document.createElement('img');
+//     $imgIssues.src = item.Image;
 
-    let $h1Issues = document.createElement('h1');
-    $h1Issues.innerHTML = '1990';
+//     let $h1Issues = document.createElement('h1');
+//     $h1Issues.innerHTML = '1990';
 
-    let $pIssuses = document.createElement('p');
-    $pIssuses.innerHTML = 'Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.'
+//     let $pIssuses = document.createElement('p');
+//     $pIssuses.innerHTML = 'Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.'
 
-    $liIssues.append($imgIssues, $h1Issues, $pIssuses);
-    $ulIssues.append($liIssues);
-}
+//     $liIssues.append($imgIssues, $h1Issues, $pIssuses);
+//     $ulIssues.append($liIssues);
+// }
+
+
+// ///////
+
+// let $nameEvent = document.getElementsByClassName('name')[0];
+// let $textNameEvent = document.createElement('h4');
+// $textNameEvent.innerHTML = 'Tên sự kiện: ' + '';
+// $nameEvent.append($textNameEvent);
+
+// let $dateEvent = document.getElementsByClassName('date')[0];
+// let $textDateEvent = document.createElement('h4');
+// $textDateEvent.innerHTML = 'Ngày: ' + '';
+// $dateEvent.append($textDateEvent);
+
+
+// let $imgEvent = document.getElementsByClassName('image')[0];
+// let $imageEvent = document.createElement('img');
+// $imageEvent.src = '';
+// $imageEvent.alt = 'Image';
+// $imgEvent.append($imageEvent);
+
+// let $locationEvent = document.getElementsByClassName('location')[0];
+// let $textLoactionEvent = document.createElement('h5');
+// $textLoactionEvent.innerHTML = 'Địa điểm: ' + '';
+// $locationEvent.append($textLoactionEvent);
+
+// let $introductionEvent = document.getElementsByClassName('introduction')[0];
+// let $textIntroductionEvent = document.createElement('p');
+// $textIntroductionEvent.innerHTML = 'Khái quát chung: ' + '';
+// $introductionEvent.append($textIntroductionEvent);
+
+// let $resultEvent = document.getElementsByClassName('result')[0];
+// let $textResultEvent = document.createElement('p');
+// $textResultEvent.innerHTML = 'Kết quả và ý nghĩa lịch sử: ' + ''
+// $resultEvent.append($textResultEvent);
+
 
 
 ///////
+
+//Event Details 2
+let id = localStorage.getItem('id');
+let id1 = await db.collection('characters').doc(id).get();
+console.log('don: ', id1.data());
+let getChar = id1.data();
+
 
 let $nameEvent = document.getElementsByClassName('name')[0];
 let $textNameEvent = document.createElement('h4');
@@ -54,7 +97,7 @@ $dateEvent.append($textDateEvent);
 
 let $imgEvent = document.getElementsByClassName('image')[0];
 let $imageEvent = document.createElement('img');
-$imageEvent.src = '';
+$imageEvent.src = 'https://upload.wikimedia.org/wikipedia/commons/4/49/C%E1%BB%8Dc_B%E1%BA%A1ch_%C4%90%E1%BA%B1ng.jpg';
 $imageEvent.alt = 'Image';
 $imgEvent.append($imageEvent);
 
@@ -74,5 +117,38 @@ $textResultEvent.innerHTML = 'Kết quả và ý nghĩa lịch sử: ' + ''
 $resultEvent.append($textResultEvent);
 
 
+//Timeline
+let $timeline = document.getElementsByClassName('life-character')[0];
 
-///////
+for (let item of getChar.details) {
+    let $groupTimeline = document.createElement('div');
+    $groupTimeline.className = 'group-life';
+
+    let $time = document.createElement('div');
+    $time.className = 'time-life';
+
+    let $timeText = document.createElement('h2');
+    $timeText.innerHTML = item.date;
+
+    let $divImageTimeline = document.createElement('div');
+    $divImageTimeline.className = '';
+
+
+    //Picture
+    let $imgTimeline = document.createElement('img');
+    $imgTimeline.className = 'img-life';
+    $imgTimeline.src = 'https://upload.wikimedia.org/wikipedia/commons/4/49/C%E1%BB%8Dc_B%E1%BA%A1ch_%C4%90%E1%BA%B1ng.jpg';
+    $imgTimeline.alt = 'Img';
+
+    let $detailTimeline = document.createElement('div');
+    $detailTimeline.className = 'details';
+
+    let $textDetailTimeline = document.createElement('p');
+    $textDetailTimeline.innerHTML = item.description;
+
+    $time.append($timeText);
+    $divImageTimeline.append($imgTimeline);
+    $detailTimeline.append($textDetailTimeline);
+    $groupTimeline.append($time, $divImageTimeline, $detailTimeline);
+    $timeline.append($groupTimeline);
+}
