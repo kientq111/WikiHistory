4e4b3f0afb75b8f4e3381c1a0982e67d92c6c032
KenKen <kiennahe130771@fpt.edu.vn>
2021-10-02 10:26:46 +0700

    commit típ típ

--- view/script/admin_CreateEvent.js
+++ view/script/admin_CreateEvent.js
@@ -137,8 +137,8 @@ function createStage() {
 async function saveEvent() {
     let data = {
         name: document.getElementById('eventName').value,
-        firstDay: document.getElementById('startDate').value,
-        lastDay: document.getElementById('endDate').value,
+        firstday: document.getElementById('startDate').value,
+        lastday: document.getElementById('endDate').value,
         location: document.getElementById('location').value,
         general: document.getElementById('general').value,
         result: document.getElementById('result').value,
